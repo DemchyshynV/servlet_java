bca5e45eee4ad6b01824f144d01621f6c685cc02
RoksolianaMatseliukh <matseliukhroksoliana100@gmail.com>
2020-12-10 22:02:24 +0200

    add regex

--- hw4/src/products/StartPoint.java
+++ hw4/src/products/StartPoint.java
@@ -7,7 +7,7 @@ public class StartPoint {
     public static void main(String[] args) {
 
         Store store = new Store();
-        Product product = new Product(9, "app0e", 0.5, ProductType.FRUITS, "Ukraine", "red");
+        Product product = new Product(9, "app7e", 0.5, ProductType.FRUITS, "Ukraine", "red");
 
         store.addProduct(product);
 
